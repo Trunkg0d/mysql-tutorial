2085786add804d085f6e4405832afdad7d8a5294
unknown <trungsanglong@gmail.com>
2022-07-28 22:42:08 +0700

    Update

---
+++ Delete.sql
@@ -0,0 +1,9 @@
+/*
+
+The DELETE statement is used to delete existing records in a table.
+DELETE Syntax:
+DELETE FROM table_name WHERE condition;
+
+*/
+-- ex:
+DELETE FROM Customers WHERE CustomerName='Cardinal';
